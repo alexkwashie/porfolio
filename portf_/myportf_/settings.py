643101d157cd1a7672e2efc2f058d2ed8429e6d7
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-03-04 14:30:35 +0000

    Javascript All working now

--- portf_/myportf_/settings.py
+++ portf_/myportf_/settings.py
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/Users/alexkwashie/Desktop/WorkingFiles/portfolio/portf_/portf_App/static/'
+STATIC_URL = '/static/'
+
