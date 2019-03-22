1f3ce80042071eaa8d7c5403be46abf2aba3f014
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-03-22 18:24:15 +0000

    reverting to static a page

--- portf_/portf_App/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfAppConfig(AppConfig):
-    name = 'portf_App'
--- portf_/portf_App/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-from django.conf.urls.static import static
-
-
-urlpatterns = [
-    path ('', views.home, name = 'home'),
-]
